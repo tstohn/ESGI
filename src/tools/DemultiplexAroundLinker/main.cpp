7afb03b077eed1d2a10bd01bf3ff6146ead5f359
Tim Stohn <64840677+tstohn@users.noreply.github.com>
2022-10-19 15:09:31 +0200

    Update main.cpp

--- src/tools/DemultiplexAroundLinker/main.cpp
+++ src/tools/DemultiplexAroundLinker/main.cpp
@@ -6,8 +6,15 @@
 #include <regex>
 #include <thread>
 
-#include <boost/program_options.hpp>
 #include <boost/program_options/options_description.hpp>
+#include <boost/program_options/positional_options.hpp>
+#include <boost/program_options/parsers.hpp>
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/cmdline.hpp>
+#include <boost/program_options/errors.hpp>
+#include <boost/program_options/option.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/version.hpp>
 
 #include "BarcodeProcessingHandler.hpp"
 #include "BarcodeMapping.hpp"
